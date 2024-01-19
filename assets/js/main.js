b62cfac5edd39285a3aee917670e0ef6f236c605
marcovittoriozacca <zaccamarco01@gmail.com>
2024-01-19 14:19:09 +0100

    feat: array of object with the team's info

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+const team = [
+    {
+        name: 'Wane Barnett',
+        role: 'Founder & CEO',
+        photo: './assets/img/wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        photo: './assets/img/angela-caroll-chief-editor.jpg',
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        photo: './assets/img/walter-gordon-office-manager.jpg',
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        photo: './assets/img/angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        photo: './assets/img/scott-estrada-developer.jpg',
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        photo: './assets/img/walter-gordon-office-manager.jpg',
+    },
+]
+
