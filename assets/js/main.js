d8f874bdd9d5144d14331128e3736878c0b79b2d
marcovittoriozacca <zaccamarco01@gmail.com>
2024-01-19 15:22:32 +0100

    feat: element inside cards with style + favicon. Milestone completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,13 +28,28 @@ const team = [
     {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
-        photo: './assets/img/walter-gordon-office-manager.jpg',
+        photo: './assets/img/barbara-ramos-graphic-designer.jpg',
     },
 ]
 
-//ciclo che mostra l'intero oggetto in console e nel DOM stampato momentaneamente nel body
-for(name in team){
-    console.log(team[name]);
-    document.body.innerHTML += `${team[name].name}, ${team[name].role}, ${team[name].photo}<br>`
-}
+const mainHtml = document.querySelector('main .container .row');
 
+//ciclo che mostra l'intero oggetto in console e nel DOM stampato momentaneamente nel body
+//usiamo name per iterare il ciclo con gli elementi in base a quante volte è presente la key "name" negli oggetti. es: 5 oggetti con "nome", il ciclo viene eseguito 5 volte.
+//ha anche funzione di indice quindi la usiamo come valore di indice nell'array per stampare i valori relativi a quello specifico oggetto in ordine
+for(name in team){
+    //per vedere gli oggetti dell'array in console
+    // console.log(team[name]);
+
+    mainHtml.innerHTML += `
+        <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="card text-center">
+                <img class="card-img-top p-1 " src="${team[name].photo}" alt="team member"/>
+                <div class="card-body">
+                    <h4 class="card-title fw-bolder">${team[name].name}</h4>
+                    <p class="card-text fw-bold ">${team[name].role}</p>
+                </div>
+            </div>
+        </div>
+    `
+}
